f3538cc6ae3dcd2cb6664b851bf0513761360225
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2026-02-18 08:38:27 +0100

    First config and dummy static.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hola mundo!");
+const express = require('express');
+
+
+
+const app = express();
+
+
+
+//Routes
+
+/* app.get("/", (req, res) => {
+    res.json({
+        ok: true
+    })
+}) */
+
+app.use(express.static('public'));
+
+app.listen(3001, () => {
+    console.log(`Server running in port ${3001}`);
+})
+
+
+
+
+
