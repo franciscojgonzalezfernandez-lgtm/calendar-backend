8b6e9a3b657bd1772fa2fa3d34a3e1a3a0464838
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2026-02-25 08:42:56 +0100

    BASIC CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./database/config');
 require('dotenv').config();
+const cors = require('cors')
 
 
 
@@ -10,13 +11,17 @@ const app = express();
 
 connectDB();
 
+app.use(cors());
+
 
 
 // Reading and parsing
 app.use(express.json());
 
 
+app.use(express.static('public'));
 
+//CRUD operations
 
 //Routes
 // TODO Auth, New user, login, renew token
@@ -24,11 +29,10 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'))
 
 
-//CRUD
 
 
 
-app.use(express.static('public'));
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running in port ${3001}`);
